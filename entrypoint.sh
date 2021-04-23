a996640a2ef3666980a11830436eeb4448401efd
rudenav <ravmailbox@gmail.com>
2021-04-23 11:19:25 +0300

    updating the action

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,10 +11,18 @@ ls -la $SCRIPT_PATH
 
 cat $SCRIPT_PATH
 
-if [ -x $SCRIPT_PATH ]
-then
-  echo "executable"
+if [ -z $SCRIPT_PATH ]; then
+  echo "File path is not defined"
+  return 127
+fi
+
+if [ ! -f $SCRIPT_PATH ]; then
+  echo "File not found"
+  return 127
+fi
+
+if [ -x $SCRIPT_PATH ]; then
   ./$SCRIPT_PATH
 else
-  echo "not executable"
+  . $SCRIPT_PATH
 fi
